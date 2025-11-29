1035eab77b9ecb38e9e7eab1ddbcd5f4510bdaf0
awaisirshad1 <awaisirshad1@icloud.com>
2025-11-28 22:04:25 -0500

    added text transitions with button

--- app/src/theme/ThemeSelector.tsx
+++ app/src/theme/ThemeSelector.tsx
@@ -1,4 +1,4 @@
-import { Component, createSignal } from "solid-js";
+import { Component, createSignal, Show } from "solid-js";
 import "./themeselector.css"
 import { useTheme } from "./useTheme";
 
@@ -15,7 +15,16 @@ export const ThemeSelector: Component = () => {
   return (
     <div class="theme-selector-container">
       <button class="theme-button" onClick={toggle}>
-        <div class={`circle ${slider() ? "slide" : ""}`}/>
+        <div class={`circle ${slider() ? "slide" : ""}`}>
+        </div>
+        <div class="text-wrapper">
+            <Show when={theme() === "dark"}>
+              <div class="slide-in-right">dark</div>
+            </Show>
+            <Show when={theme() === "light"}>
+              <div class="slide-in-left">light</div>
+            </Show>
+          </div>
       </button>
     </div>
   );
